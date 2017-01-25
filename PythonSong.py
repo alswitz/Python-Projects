59c4d48a7293c31d74e023f69264ee8a94ae33d9
alswitz <alexanderswitz09@gmail.com>
2017-01-24 21:31:23 -0500

    Added second assignment. Current WiP

---
+++ PythonSong.py
@@ -0,0 +1,17 @@
+#Python Song Creator
+#Version 1.0
+#By Alexander Switzer
+
+theSong = []
+increment = 1
+verse1 = raw_input("Enter the first verse: ")
+verse2 = raw_input("Enter the second verse: ")
+verse3 = raw_input("Enter the third verse: ")
+verse4 = raw_input("Enter the fourth verse: ")
+
+for x in range(0, 4):
+    theSong.append(verse)
+
+print theSong
+
+question = raw_input("Did it work?")
